76fea3f61dd1a82632f1fefc195ffd1fe414e732
hutnichs <hutnichs@gmail.com>
2026-07-31 22:21:01 +0300

    a

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,16 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  switch (days) {
+    case 2:
+      return 80;
+    case 3:
+      return 100;
+    case 6:
+      return 220;
+    case 7:
+      return 230;
+  }
 }
 
 module.exports = calculateRentalCost;
